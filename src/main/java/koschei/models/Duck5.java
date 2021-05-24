ffe8e836be7c5ad9d7323c82fdf3f84c4ac62287
Станислав <yastas593@mail.ru>
2021-05-24 23:24:14 +0300

    read revision 1

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    public Egg6 egg6;
+
+    @Autowired
+    public void setEgg(Egg6 egg6){
+        this.egg6 = egg6;
+    }
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg6.toString();
     }
 }
